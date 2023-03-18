38e47466e223f2992121160995041703a212fad9
simplysubedi <send2arjunsubedi@gmail.com>
2023-03-18 16:45:43 -0500

    entity parameters defined

--- src/main/java/edu/miu/cs/cs544/model/LocationType.java
+++ src/main/java/edu/miu/cs/cs544/model/LocationType.java
@@ -1,4 +1,6 @@
 package edu.miu.cs.cs544.model;
 
 public enum LocationType {
+    DINING_HALL, MEDITATION_HALL, FLYING_HALL, CLASSROOM, GYMNASIUM, DORMITORY
+
 }
--- src/main/java/edu/miu/cs/cs544/model/Timeslot.java
+++ src/main/java/edu/miu/cs/cs544/model/Timeslot.java
@@ -1,4 +1,24 @@
 package edu.miu.cs.cs544.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import java.sql.Time;
+
+@Entity
+@Data
+@Table(name="Timeslot")
 public class Timeslot {
+    @Id
+    private int id;
+    @JoinColumn(name="StartTime")
+    private Time startTime;
+    @JoinColumn(name="StartTime")
+    private Time endTime;
+    @JoinColumn(name="DayOfWeek")
+    private Day day;
+
 }
