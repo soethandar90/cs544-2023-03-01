2c4a5a59b4146df9329c4d848ec545f6dc757686
615175 <feven.tekleab@miu.edu>
2023-03-18 16:52:35 -0500

    transaction entity change

--- src/main/java/edu/miu/cs/cs544/model/Transaction.java
+++ src/main/java/edu/miu/cs/cs544/model/Transaction.java
@@ -2,17 +2,25 @@ package edu.miu.cs.cs544.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Data
-@Table
+@Table(name = "Transaction")
 public class Transaction {
     @Id
     @GeneratedValue
     private int id;
+    private LocalDate timeStamp;
+    private String transactionType;
+    @Column(name = "Location")
+    private Location locaton;
+    @Column(name = "Plan")
+    private Plan plan;
+    @Column(name = "Transaction")
+    private Transaction transaction;
+    @Column(name = "Badge")
+    private Badge badge;
 
 }
