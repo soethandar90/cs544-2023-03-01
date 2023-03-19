7771ddf845fd529565ec39f940089f362425b1a4
Mohammed Fahim Uddin <fahim.uddin@comparcelub.com.au>
2023-03-19 16:39:32 -0500

    Added role service controller repository

---
+++ src/main/java/edu/miu/cs/cs544/controller/RoleController.java
@@ -0,0 +1,31 @@
+package edu.miu.cs.cs544.controller;
+
+
+import edu.miu.cs.cs544.model.Role;
+import edu.miu.cs.cs544.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    @Autowired
+    RoleService roleService;
+
+    @PostMapping("/")
+    public Role addOneRole(@RequestBody Role role) {
+        roleService.addOneRole(role);
+
+        return role;
+    }
+    @PutMapping("/{id}")
+    public Role updateOneRoleByRoleId(@PathVariable Integer id,@RequestParam String name) {
+        Role role = roleService.updateOneRoleByRoleId(id,name);
+
+        return role;
+    }
+    @DeleteMapping("/{id}")
+    public String deleteOneRoleByRoleId(@PathVariable Integer id){
+        return roleService.deleteOneRoleByRoleId(id);
+    }
+}
