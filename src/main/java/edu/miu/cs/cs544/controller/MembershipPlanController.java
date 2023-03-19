5f0b79da71575dee84bb288b0c6773edb61c5937
Feven Dawit <fevendawit72@gmail.com>
2023-03-19 18:11:53 -0500

    membership plan crud

---
+++ src/main/java/edu/miu/cs/cs544/controller/MembershipPlanController.java
@@ -0,0 +1,46 @@
+package edu.miu.cs.cs544.controller;
+
+import edu.miu.cs.cs544.model.Location;
+import edu.miu.cs.cs544.model.MembershipPlan;
+import edu.miu.cs.cs544.service.LocationService;
+import edu.miu.cs.cs544.service.MembershipPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/membershipPlans")
+public class MembershipPlanController {
+
+
+    @Autowired
+    private MembershipPlanService membershipPlanService;
+
+    @GetMapping
+    public List<MembershipPlan> findAll() {
+        return membershipPlanService.findAllMembershipPlanOfmemberByMemberId();
+    }
+
+    @PostMapping
+    public void addOnePlan(@RequestBody MembershipPlan membershipPlan) {
+        membershipPlanService.addOnePlan(membershipPlan);
+    }
+
+    @PutMapping("/{id}")
+    public void updateOnePlan(@PathVariable Integer id,@RequestBody MembershipPlan membershipPlan) {
+        //membershipPlanService.updateOnePlanByPlanId(membershipPlan);
+    membershipPlanService.updateOnePlanByPlanId(membershipPlan);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOnePlan(@PathVariable int id) {
+        membershipPlanService.deleteOnePlan(id);
+    }
+
+
+//
+//
+}
---
+++ src/main/java/edu/miu/cs/cs544/model/MembershipPlan.java
@@ -0,0 +1,37 @@
+package edu.miu.cs.cs544.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="MembershipPlan")
+public class MembershipPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="planId")
+    private int planId;
+
+    @Column(name="PlanName")
+    private String name;
+
+    @Column(name="planDescription")
+    private  String planDescription;
+
+    @OneToMany
+    @JoinTable(name="plan_location",
+            joinColumns = {@JoinColumn(name="planId")},
+            inverseJoinColumns = {@JoinColumn(name="locationId")})
+    private List<Location> locations;
+
+    @OneToMany
+    @JoinTable(name="plan_role",
+            joinColumns = {@JoinColumn(name="planId")},
+            inverseJoinColumns = {@JoinColumn(name="roleId")})
+    private List<Role> roles;
+
+
+
+}
