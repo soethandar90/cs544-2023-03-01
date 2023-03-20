c8317485720da00e08420ee681ca42e1ed847549
Soe Thandar <615197@cs.mum.edu>
2023-03-20 06:18:52 -0500

    Membership commit

---
+++ src/main/java/edu/miu/cs/cs544/service/imp/MembershipServiceImpl.java
@@ -0,0 +1,38 @@
+package edu.miu.cs.cs544.service.imp;
+
+import edu.miu.cs.cs544.model.Membership;
+import edu.miu.cs.cs544.repository.MembershipRepository;
+import edu.miu.cs.cs544.service.MembershipService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class MembershipServiceImpl implements MembershipService {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Override
+    public void addOneMembership(Membership membership) {
+        membershipRepository.save(membership);
+    }
+
+    @Override
+    public void updateOneMembershipByMembershipId(Membership membership) {
+        membershipRepository.save(membership);
+    }
+
+    @Override
+    public void deleteOneMembershipByMembershipId(int membershipId) {
+        membershipRepository.deleteById(membershipId);
+    }
+
+    @Override
+    public Membership findAllMembershipsOfOneMemberByMemberId(int memberId) {
+       //return membershipRepository.findAllByMemberId(memberId);
+        return null;
+    }
+}
