556b3f5f3d3e40ab6f2c5227c35e241731f98881
Mohammed Fahim Uddin <fahim.uddin@comparcelub.com.au>
2023-03-19 21:24:16 -0500

    added timeslot service,repository,controller

---
+++ src/main/java/edu/miu/cs/cs544/repository/TimeslotRepository.java
@@ -0,0 +1,7 @@
+package edu.miu.cs.cs544.repository;
+
+import edu.miu.cs.cs544.model.Timeslot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeslotRepository extends JpaRepository<Timeslot, Integer> {
+}
