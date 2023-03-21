f6048e8acf237a53a9bd67f83e28e6fe6c7f1aa1
AsmelashGua <azmeabrha@gmail.com>
2023-03-21 16:06:46 -0500

    BadgeTransactionController modification

--- src/main/java/edu/miu/cs/cs544/controller/BadgeTransactionController.java
+++ src/main/java/edu/miu/cs/cs544/controller/BadgeTransactionController.java
@@ -4,10 +4,7 @@ import edu.miu.cs.cs544.model.Badge;
 import edu.miu.cs.cs544.model.BadgeTransaction;
 import edu.miu.cs.cs544.service.BadgeTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,16 +16,16 @@ public class BadgeTransactionController {
     private BadgeTransactionService badgeTransactionService;
 
  @GetMapping
- public Badge filterActiveBadgeByMemberId(int memberId) {
+ public Badge filterActiveBadgeByMemberId(@PathVariable int memberId) {
      return badgeTransactionService.filterActiveBadgeByMemberId(memberId);
  }
  @GetMapping("/all")
- public List<BadgeTransaction> findAllBadgeTransactionByMemberId(int memberId) {
+ public List<BadgeTransaction> findAllBadgeTransactionByMemberId(@PathVariable int memberId) {
      return badgeTransactionService.findAllBadgeTransactionByMemberId(memberId);
  }
 
     @PostMapping
-    public void addOneTransaction(BadgeTransaction badgeTransaction) {
+    public void addOneTransaction(@RequestBody BadgeTransaction badgeTransaction) {
         badgeTransactionService.addOneTransaction(badgeTransaction);
     }
 
